abf534921bc646a5f4e8f62f5383b1f51f59a2a3
Shai Aharon <shai.aharon1@msmail.ariel.ac.il>
2020-11-22 00:18:29 +0200

    test cherry-picking

--- codes/test.py
+++ codes/test.py
@@ -63,83 +63,87 @@ for test_loader in test_loaders:
     util.mkdir(dataset_dir)
 
     model_dict = torch.load(opt['path']['pretrain_model_G'])
-    interp_dict = model_dict.copy()
-    for k, v in model_dict.items():
-        if k.find('transformer') >= 0:
-            interp_dict[k] = v * test_loader.dataset.opt['coef']
-    model.update(interp_dict)
+    base_alpha = test_loader.dataset.opt['coef']
+    alpha_lst = np.unique([x + base_alpha for x in [-.5, -.25, 0, .25, .5]])
+    best_psnr = -1
+    best_ssim = -1
+    for alpha in alpha_lst:
+        interp_dict = model_dict.copy()
+        for k, v in model_dict.items():
+            if k.find('transformer') >= 0:
+                interp_dict[k] = v * alpha
+        model.update(interp_dict)
 
-    test_results = OrderedDict()
-    test_results['psnr'] = []
-    test_results['ssim'] = []
-    test_results['psnr_y'] = []
-    test_results['ssim_y'] = []
+        test_results = OrderedDict()
+        test_results['psnr'] = []
+        test_results['ssim'] = []
+        test_results['psnr_y'] = []
+        test_results['ssim_y'] = []
 
-    c = 0
-    for data in test_loader:
-        c += 1
-        if c > 5:
-            break
-        need_HR = False if test_loader.dataset.opt['dataroot_HR'] is None else True
+        for data in test_loader:
+            need_HR = False if test_loader.dataset.opt['dataroot_HR'] is None else True
 
-        model.feed_data(data, need_HR=need_HR)
-        img_path = data['LR_path'][0]
-        img_name = os.path.splitext(os.path.basename(img_path))[0]
+            model.feed_data(data, need_HR=need_HR)
+            img_path = data['LR_path'][0]
+            img_name = os.path.splitext(os.path.basename(img_path))[0]
 
-        model.test()  # test
-        visuals = model.get_current_visuals(need_HR=need_HR)
+            model.test()  # test
+            visuals = model.get_current_visuals(need_HR=need_HR)
 
-        sr_img = util.tensor2img(visuals['SR'])  # uint8
+            sr_img = util.tensor2img(visuals['SR'])  # uint8
 
-        # save images
-        suffix = opt['suffix']
-        if suffix:
-            save_img_path = os.path.join(dataset_dir, img_name + suffix + '.png')
-        else:
-            save_img_path = os.path.join(dataset_dir, img_name + '.png')
-        util.save_img(sr_img, save_img_path)
-
-        # calculate PSNR and SSIM
-        if need_HR:
-            gt_img = util.tensor2img(visuals['HR'])
-            gt_img = gt_img / 255.
-            sr_img = sr_img / 255.
-
-            crop_size = opt['crop_size']
-            if crop_size > 0:
-                sr_img = sr_img[crop_size:-crop_size, crop_size:-crop_size, :]
-                gt_img = gt_img[crop_size:-crop_size, crop_size:-crop_size, :]
-
-            psnr = util.calculate_psnr(sr_img * 255, gt_img * 255)
-            ssim = util.calculate_ssim(sr_img * 255, gt_img * 255)
-            test_results['psnr'].append(psnr)
-            test_results['ssim'].append(ssim)
-
-            if gt_img.shape[2] == 3:  # RGB image
-                sr_img_y = bgr2ycbcr(sr_img, only_y=True)
-                gt_img_y = bgr2ycbcr(gt_img, only_y=True)
-                if crop_size > 0:
-                    sr_img_y = sr_img_y[crop_size:-crop_size, crop_size:-crop_size]
-                    gt_img_y = gt_img_y[crop_size:-crop_size, crop_size:-crop_size]
-                psnr_y = util.calculate_psnr(sr_img_y * 255, gt_img_y * 255)
-                ssim_y = util.calculate_ssim(sr_img_y * 255, gt_img_y * 255)
-                test_results['psnr_y'].append(psnr_y)
-                test_results['ssim_y'].append(ssim_y)
-                # logger.info('{:20s} - PSNR: {:.6f} dB; SSIM: {:.6f}; PSNR_Y: {:.6f} dB; SSIM_Y: {:.6f}.' \
-                #             .format(img_name, psnr, ssim, psnr_y, ssim_y))
+            # save images
+            suffix = opt['suffix']
+            if suffix:
+                save_img_path = os.path.join(dataset_dir, img_name + suffix + '.png')
             else:
-                logger.info('{:20s} - PSNR: {:.6f} dB; SSIM: {:.6f}.'.format(img_name, psnr, ssim))
-        else:
-            logger.info(img_name)
+                save_img_path = os.path.join(dataset_dir, img_name + '.png')
+            util.save_img(sr_img, save_img_path)
 
-    if need_HR:  # metrics
+            # calculate PSNR and SSIM
+            if need_HR:
+                gt_img = util.tensor2img(visuals['HR'])
+                gt_img = gt_img / 255.
+                sr_img = sr_img / 255.
+
+                crop_size = opt['crop_size']
+                if crop_size > 0:
+                    sr_img = sr_img[crop_size:-crop_size, crop_size:-crop_size, :]
+                    gt_img = gt_img[crop_size:-crop_size, crop_size:-crop_size, :]
+
+                psnr = util.calculate_psnr(sr_img * 255, gt_img * 255)
+                ssim = util.calculate_ssim(sr_img * 255, gt_img * 255)
+                test_results['psnr'].append(psnr)
+                test_results['ssim'].append(ssim)
+
+                # if gt_img.shape[2] == 3:  # RGB image
+                #     sr_img_y = bgr2ycbcr(sr_img, only_y=True)
+                #     gt_img_y = bgr2ycbcr(gt_img, only_y=True)
+                #     if crop_size > 0:
+                #         sr_img_y = sr_img_y[crop_size:-crop_size, crop_size:-crop_size]
+                #         gt_img_y = gt_img_y[crop_size:-crop_size, crop_size:-crop_size]
+                #     psnr_y = util.calculate_psnr(sr_img_y * 255, gt_img_y * 255)
+                #     ssim_y = util.calculate_ssim(sr_img_y * 255, gt_img_y * 255)
+                #     test_results['psnr_y'].append(psnr_y)
+                #     test_results['ssim_y'].append(ssim_y)
+                #     # logger.info('{:20s} - PSNR: {:.6f} dB; SSIM: {:.6f}; PSNR_Y: {:.6f} dB; SSIM_Y: {:.6f}.' \
+                #     #             .format(img_name, psnr, ssim, psnr_y, ssim_y))
+                # else:
+                #     logger.info('{:20s} - PSNR: {:.6f} dB; SSIM: {:.6f}.'.format(img_name, psnr, ssim))
+            else:
+                logger.info(img_name)
+
+        if need_HR:  # metrics
+            best_psnr = np.max(best_psnr, sum(test_results['psnr']) / len(test_results['psnr']))
+            best_ssim = np.max(best_ssim, sum(test_results['ssim']) / len(test_results['ssim']))
+
+    if need_HR:
         # Average PSNR/SSIM results
-        ave_psnr = sum(test_results['psnr']) / len(test_results['psnr'])
-        ave_ssim = sum(test_results['ssim']) / len(test_results['ssim'])
         logger.info('----Average PSNR/SSIM results for {}----\n\tPSNR: {:.6f} dB; SSIM: {:.6f}\n' \
-                    .format(test_set_name, ave_psnr, ave_ssim))
-        if test_results['psnr_y'] and test_results['ssim_y']:
-            ave_psnr_y = sum(test_results['psnr_y']) / len(test_results['psnr_y'])
-            ave_ssim_y = sum(test_results['ssim_y']) / len(test_results['ssim_y'])
-            logger.info('----Y channel, average PSNR/SSIM----\n\tPSNR_Y: {:.6f} dB; SSIM_Y: {:.6f}\n' \
-                        .format(ave_psnr_y, ave_ssim_y))
+                    .format(test_set_name, best_psnr, best_ssim))
+
+        # if test_results['psnr_y'] and test_results['ssim_y']:
+        # ave_psnr_y = sum(test_results['psnr_y']) / len(test_results['psnr_y'])
+        # ave_ssim_y = sum(test_results['ssim_y']) / len(test_results['ssim_y'])
+        # logger.info('----Y channel, average PSNR/SSIM----\n\tPSNR_Y: {:.6f} dB; SSIM_Y: {:.6f}\n' \
+        #             .format(ave_psnr_y, ave_ssim_y))
